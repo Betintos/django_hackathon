0e4f7c569f4b8d6fce595d2ae2ea5733e3682405
Betintos <qed.beknazar@gmail.com>
2024-04-24 14:49:13 +0600

    created account app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #libs
+    'rest_framework'
+    #apps
+    'account',
 ]
 
 MIDDLEWARE = [
