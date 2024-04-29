31810c779467982c823549935d4839d04bb630cd
[Jormungand] <marlen.python@bk.ru>
2024-04-29 13:49:47 +0600

    marlen

---
+++ magazin/account/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    list_filter = ('is_active',)
+    search_fields = ('email',)
+
+
+admin.site.register(User, UserAdmin)
+
